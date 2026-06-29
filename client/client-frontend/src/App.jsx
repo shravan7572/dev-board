73cdb5cd1d8fb00a4d8ced21ca0a0518784d9535
Shravan Choudhary <smchoudhary19093@gmail.com>
2026-06-29 10:11:17 +0530

    feat: implemented a protected route so anyone can't go to dashboard

--- client/client-frontend/src/App.jsx
+++ client/client-frontend/src/App.jsx
@@ -1,15 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import Dashboard from "./pages/dashboard"
 import Landing from "./pages/landing"
 import Profilepage from "./pages/publicprofile"
 import NotFound404 from"./pages/404page"
 
+    function ProtectedRoute({ children }) {
+    const token = localStorage.getItem("token")
+    const navigate = useNavigate()
+
+    if(!token) {
+        navigate("/")
+        return null
+    }
+
+    return children
+}
+
 function App() {
+
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Landing />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/dashboard" element={
+                    <ProtectedRoute>
+                    <Dashboard />
+                    </ProtectedRoute>} />
                 <Route path="/:username" element={<Profilepage />} />
                 <Route path="*" element={<NotFound404 />} />
             </Routes>
